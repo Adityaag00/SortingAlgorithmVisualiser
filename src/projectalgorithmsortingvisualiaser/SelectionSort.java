48c4e2d8b690f745252ba2d80dbfa04ddcc21d05
Adityaag00 <adityaagrawal399@gmail.com>
2021-01-31 23:23:44 +0530

    first

---
+++ src/projectalgorithmsortingvisualiaser/SelectionSort.java
@@ -0,0 +1,28 @@
+
+package projectalgorithmsortingvisualiaser;
+
+/**
+ *
+ * @author Aditya
+ */
+public class SelectionSort {
+    
+    pnlAnimation ani;
+    
+    public SelectionSort(pnlAnimation ani) {
+      this.ani = ani;
+    }
+    
+    public void sort() {
+        int min=0;
+        for(int i=0;i<ani.getLength()-1;i++){
+            min=i;
+            for(int j=i+1;j<ani.getLength();j++) {
+                if(ani.getValue(j)<ani.getValue(min))
+                    min=j;
+                
+            }
+            ani.swap(min, i);
+        }
+    }
+}
